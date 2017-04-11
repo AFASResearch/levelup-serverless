e5494a9b5c67aecaf924a3a8c9603e42d12cfe3f
Peter Westendorp <p.westendorp@afas.nl>
2017-04-11 22:33:11 +0200

    first version bootstrap LevelUp session about serverless architectures

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# levelup-serverless
-Bootstrap for LevelUp Meetup about serverless architectures
+# LevelUp
+This is a bootstrap for the LevelUp Meetup about serverless architectures.
+
+## Goals
+The goal is to make a registration page for this Meetup (similar to meetup.com). 
+
+- Store names and email-addresses in Firebase.
+- Show a list of attendees on the page
+- Bonus: send a 'welcome e-mail' for each successfull registration
+
+
+## Technologies used
+Bundling is done using [Webpack 2](https://webpack.js.org/): 
+
+Authentication is done with [Firebase](https://firebase.google.com) and [FirebaseUI](https://github.com/firebase/firebaseui-web)
+
+
+## Start the project
+- `npm install`
+- `npm run dev`
---
+++ app/main.js
@@ -0,0 +1,75 @@
+let firebase = require('firebase/app');
+require('firebase/auth');
+// require('firebase/database');
+let firebaseui = require('firebaseui');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCBLXn5d6QLFj1c2qCn5j3XvPTpkhhqZyw',
+  authDomain: 'levelup-ee96f.firebaseapp.com',
+  databaseURL: 'https://levelup-ee96f.firebaseio.com',
+  projectId: 'levelup-ee96f',
+  storageBucket: 'levelup-ee96f.appspot.com',
+  messagingSenderId: '88343316553'
+});
+
+(() => {
+  let loginContainerElement = document.getElementById('firebaseui-auth-container');
+  let registrationFormElement = document.getElementById('registration-form');
+  let signOutButtonElement = document.getElementById('sign-out-button');
+
+  let errorHandler = (error) => console.error('something went wrong', error);
+
+  let showLogin = () => {
+    loginContainerElement.style.display = 'block';
+    signOutButtonElement.style.display = 'none';
+    registrationFormElement.style.display = 'none';
+  };
+
+  let showForm = (user) => {
+    user.getToken().then(accessToken => {
+      console.log('signed in', user, accessToken);
+
+      loginContainerElement.style.display = 'none';
+      signOutButtonElement.style.display = 'block';
+      registrationFormElement.style.display = 'block';
+    }, errorHandler);
+  };
+
+  let handleSignOut = (evt) => {
+    evt.preventDefault();
+    firebase.auth().signOut().then(() => {
+      console.log('signed out');
+    }, errorHandler);
+  };
+
+  let initApp = () => {
+    // hide the login container initially
+    loginContainerElement.style.display = 'none';
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start(`#firebaseui-auth-container`, {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '/'
+    });
+
+    signOutButtonElement.addEventListener('click', handleSignOut);
+    // registrationFormElement.addEventListener('submit', TODO);
+
+    // this will get triggered after page is loaded as well
+    firebase.auth().onAuthStateChanged((user) => {
+      user ? showForm(user) : showLogin();
+    });
+  };
+
+  window.addEventListener('load', initApp);
+})();
